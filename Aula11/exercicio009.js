00979b52f3e9d373acbd158652dba8143b519044
WILKERSON ROSA DOS SANTOS <wilnarede@hotmail.com>
2024-05-28 12:15:10 -0300

    28-05-2024

--- Aula11/exercicio009.js
+++ Aula11/exercicio009.js
@@ -1,8 +1,13 @@
-var pais = 'Rio'
-console.log(`Vendo em ${pais}`)
-if (pais == 'Brasil'){
-    console.log(`Você é Brasileiro.`)
+function verificar(){
+    var pais = window.document.getElementById("meupais")
+    var textloc = window.document.getElementById("paistxt")
+
+    tex.innerHTML = `Sua velocidade atual é de ${vel}km/h.`
+    if (pais == 'Brasil'){
+        textloc.innerHTML
+    }
+    else{
+        console.log(`Você é gringo.`)
+    } 
 }
-else{
-    console.log(`Você é gringo.`)
-}
+
