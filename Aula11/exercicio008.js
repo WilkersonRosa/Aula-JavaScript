a28ef65fa5d9d0b556f24930a249154c851603e4
WILKERSON ROSA DOS SANTOS <wilnarede@hotmail.com>
2024-05-27 23:52:49 -0300

    27-05-2024

---
+++ Aula11/exercicio008.js
@@ -0,0 +1,7 @@
+var vel = 60.1
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou o limite de velocidade permitido. Multado!')
+}
+console.log('Dirija com segurança, utilize o cinto de segurança.')
