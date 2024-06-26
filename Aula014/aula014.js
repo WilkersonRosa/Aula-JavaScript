0863da39cc98d16c2f886614f1a4dd3516852a9a
WILKERSON ROSA DOS SANTOS <wilnarede@hotmail.com>
2024-06-26 11:56:23 -0300

    fim da aula 14

---
+++ Aula014/aula014.js
@@ -0,0 +1,10 @@
+var c = 1
+/*while(c<=10){
+    console.log(`Comprei ${c} carros.`)
+    c++
+}*/
+
+for (var b=1;b<=5;b++){
+    console.log(`Comprei ${b} bicicletas.`)
+}
+console.log('Não temos mais bicicletas no nosso estoque')
