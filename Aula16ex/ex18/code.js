443ffd9e81031d7a4bcb63e28b7ad28e4fdac180
WilkersonRosa <wilnarede@hotmail.com>
2024-07-09 17:12:57 -0300

    update

---
+++ Aula16ex/ex18/code.js
@@ -0,0 +1,11 @@
+var reg = []
+var val = document.getElementById("number")
+var contreg = document.getElementById('registro')
+
+function registrar() {
+ if (val.value.length == 0){
+   window.alert =  ('O campo não pode estar vazio!')
+ } else {
+   
+ }
+}
