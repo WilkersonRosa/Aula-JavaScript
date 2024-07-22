316d492ae07f15bcb523d447e86d1b849eb571cd
WilkersonRosa <wilnarede@hotmail.com>
2024-07-22 12:43:05 -0300

    update code.js

--- Aula16ex/ex18/code.js
+++ Aula16ex/ex18/code.js
@@ -1,8 +1,10 @@
-var reg = []
-var val = document.getElementById("number")
-var contreg = document.getElementById('registro')
 
 function registrar() {
+  
+ var reg = []
+ var val = document.getElementById("number")
+ var contreg = document.getElementById('registro') 
+  window.alert = (´exemplo´)
  if (val.value.length == 0){
    window.alert =  ('O campo não pode estar vazio!')
  } else {
