ba81aa709739e3697c21a01e609dbc96e500911b
WILKERSON ROSA DOS SANTOS <83480180+WilkersonRosa@users.noreply.github.com>
2024-07-25 17:19:09 -0300

    udpdate

--- Aula16ex/ex18/code.js
+++ Aula16ex/ex18/code.js
@@ -4,10 +4,10 @@ function registrar() {
  var reg = []
  var val = document.getElementById("number")
  var contreg = document.getElementById('registro') 
-  window.alert = (´exemplo´)
- if (val.value.length == 0){
-   window.alert =  ('O campo não pode estar vazio!')
- } else {
+  window.alert = ('exemplo')
+ //if (val.value.length == 0){
+ //  window.alert =  ('O campo não pode estar vazio!')
+ //} else {
    
- }
+ //}
 }
